1747f83a662b2083a3d25b163a3a9ece15c31e22
Anthony <anthonymcgreal@hotmail.co.uk>
2021-08-27 11:47:04 +0100

    add cors

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const app = express();
 const apiRouter = require('./routers/api.router');
 const cors = require('cors');
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/api', apiRouter);
